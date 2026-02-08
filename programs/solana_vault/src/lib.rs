08cc69d8dc2be60ae4fab25e10d592c9728c5d76
Nwaburu Emeka Christian <exellentemy@gmail.com>
2026-02-08 11:05:46 +0100

    Add a withdrawal instruction

--- programs/solana_vault/src/lib.rs
+++ programs/solana_vault/src/lib.rs
@@ -14,6 +14,10 @@ pub mod solana_vault {
     pub fn deposit(ctx: Context<Deposit>, amount: u64) -> Result<()> {
         ctx.accounts.deposit(amount)
     }
+
+    pub fn withdraw(ctx: Context<Withdraw>, amount: u64) -> Result<()> {
+        ctx.accounts.withdraw(amount)
+    }
 }
 
 #[derive(Accounts)]
@@ -89,6 +93,46 @@ impl<'info> Deposit<'info> {
         };
 
         let cpi_ctx= CpiContext::new(cpi_program, cpi_accounts);
+
+        transfer(cpi_ctx, amount)?;
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Withdraw<'info> {
+    #[account(mut)]
+    pub user: Signer<'info> ,
+
+    #[account(
+        seeds = [b"state", user.key().as_ref()],
+        bump = vault_state.state_bump
+    )]
+    pub vault_state: Account<'info, VaultState>,
+
+    #[account(
+        mut,
+        seeds = [b"vault", vault_state.key().as_ref()],
+        bump = vault_state.vault_bump
+    )]
+    pub vault: SystemAccount<'info>,
+    pub system_program: Program<'info, System>
+}
+
+impl<'info> Withdraw<'info> {
+    pub fn withdraw(&mut self, amount: u64) -> Result<()>{
+        // make a CPI call that transfers from vault to account
+        let cpi_program: AccountInfo<'_> = self.system_program.to_account_info();
+        let cpi_accounts: Transfer<'_> = Transfer {
+            from: self.vault.to_account_info(),
+            to: self.user.to_account_info(),
+        };
+
+        let vault_state_key = self.vault_state.key();
+        let seeds = &[b"vault".as_ref(), vault_state_key.as_ref(), &[self.vault_state.vault_bump]];
+        let signer = &[&seeds[..]];
+
+        let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, signer);
 
         transfer(cpi_ctx, amount)?;
         Ok(())
